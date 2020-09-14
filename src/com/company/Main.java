6abcdd9542076816ad2a88750a2f195938c4f4cf
jd-hilton <j+work@hiltons.org.uk>
2020-09-14 14:03:43 +0100

    Fizz, Buzz and FizzBuzz Functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,35 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<String> outputList = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+            if (i % 3 == 0) {
+                outputList.add("Fizz");
+            }
+            if (i % 5 == 0) {
+                outputList.add("Buzz");
+            }
+            if (i % 7 == 0) {
+                outputList.add("Bang");
+            }
+            else {
+                System.out.println(i);
+            }
+            System.out.println(outputList);
+        }
+        /*for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
+                System.out.println("FizzBuzzBang");
+            }
+            else if (i % 3 == 0 && i % 7 == 0) {
+                System.out.println("FizzBang");
+            }
+            else if (i % 5 == 0 && i % 7 == 0) {
+                System.out.println("BuzzBang");
             }
             else if (i % 3 == 0) {
                 System.out.println("Fizz");
@@ -13,9 +37,12 @@ public class Main {
             else if (i % 5 == 0) {
                 System.out.println("Buzz");
             }
+            else if (i % 7 == 0) {
+                System.out.println("Bang");
+            }
             else {
                 System.out.println(i);
             }
-        }
+        }*/
     }
 }
